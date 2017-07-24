638b5460a3a0c823e0f3df1000472e602f115d95
Benjamin Christiani <benjaminchristiani@web.de>
2017-07-25 01:18:57 +0200

    Added fragment for results and some dummy items.

--- app/src/main/java/de/christiani/benjamin/wordgen/ResultActivity.java
+++ app/src/main/java/de/christiani/benjamin/wordgen/ResultActivity.java
@@ -3,6 +3,7 @@ package de.christiani.benjamin.wordgen;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -14,10 +15,13 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("0 " + getString(R.string.toolbar_title_result));
+        toolbar.setTitle(20 + " " + getString(R.string.toolbar_title_result));
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        final ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
     }
 
 }
---
+++ app/src/main/java/de/christiani/benjamin/wordgen/ResultFragment.java
@@ -0,0 +1,26 @@
+package de.christiani.benjamin.wordgen;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+
+public class ResultFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_result, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.results, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+    }
+}
